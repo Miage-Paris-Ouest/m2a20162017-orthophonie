eeb6214af6ed6a2227de4181db82917c41e25c7c
MOURER Laurent <laurent.mourer@gmail.com>
2017-05-10 13:52:40 +0200

    Update DefaultControllerTest.php

--- src/UPOND/OrthophonieBundle/Tests/Controller/DefaultControllerTest.php
+++ src/UPOND/OrthophonieBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,7 +10,7 @@ class DefaultControllerTest extends WebTestCase
     {
     $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/login');
 
         $this->assertGreaterThan(
             0,
