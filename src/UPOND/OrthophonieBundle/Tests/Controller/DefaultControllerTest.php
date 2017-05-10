2e504a180ecf763c43e0394cd4fedbe118f243e8
MaximeAnseaume <anseaume.max@gmail.com>
2017-05-10 15:05:21 +0200

    test fonctionnels v1

--- src/UPOND/OrthophonieBundle/Tests/Controller/DefaultControllerTest.php
+++ src/UPOND/OrthophonieBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,18 +3,38 @@
 namespace UPOND\OrthophonieBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
-class DefaultControllerTest extends WebTestCase
-{
-    public function testIndex()
-    {
-    $client = static::createClient();
+class DefaultControllerTest extends WebTestCase{
 
-        $crawler = $client->request('GET', '/login');
+	private $client = null;
 
-        $this->assertGreaterThan(
-            0,
-            $crawler->filter('html:contains("Login")')->count()
-        );
+    public function setUp(){
+        $this->client = static::createClient();
     }
+	
+	private function logIn(){
+        $session = $this->client->getContainer()->get('session');
+        // the firewall context defaults to the firewall name
+        $firewallContext = 'secured_area';
+        $token = new UsernamePasswordToken('admin', null, $firewallContext);
+        $session->set('_security_'.$firewallContext, serialize($token));
+        $session->save();
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+
+    public function testFonctionnelLogin(){
+		$client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+        $this->assertGreaterThan(0,$crawler->filter('html:contains("Login")')->count());
+    }
+	
+	public function testFonctionnelIndex(){
+		$this->logIn();
+        $crawler = $this->client->request('GET', '/');
+        $this->assertGreaterThan(0,$crawler->filter('html:contains("Exercice")')->count());
+	}
 }
