2bd0787318523d9b6b5974ab3cef12d1de500969
MOURER Laurent <laurent.mourer@gmail.com>
2017-05-12 12:29:47 +0200

    Update ExerciceTest.php

--- src/UPOND/OrthophonieBundle/Tests/Controller/ExerciceTest.php
+++ src/UPOND/OrthophonieBundle/Tests/Controller/ExerciceTest.php
@@ -1,42 +1,19 @@
 <?php
-namespace Kitpages\DataGridBundle\Tests\Model;
+namespace UPOND\OrthophonieBundle\Tests\Controller;
 
-use Kitpages\DataGridBundle\Model\Field;
+use UPOND\OrthophonieBundle\Entity\Exercice;
 
-class FieldTest extends \PHPUnit_Framework_TestCase
+class ExerciceTest extends \PHPUnit_Framework_TestCase
 {
     public function testConstructor()
     {
-        $field = new Field("id");
-        $this->assertEquals($field->getFieldName(), "id");
+        $ec = new Exercice();
+        
+        $ec->setTempsExercice(1);
+                $result = ec->getTempsExercice(1);
+                $this->assertEquals(1, $result);
 
-        $field = new Field("phone", array(
-            "label" => "Phone",
-            "sortable" => true,
-            "filterable" => true,
-            "visible" => false,
-            "formatValueCallback" => function($value) { return strtoupper($value);},
-            "autoEscape" => true,
-            "translatable" => true
-        ));
-        $this->assertEquals("Phone", $field->getLabel());
-        $this->assertTrue($field->getSortable());
-        $this->assertTrue($field->getFilterable());
-        $this->assertFalse($field->getVisible());
-        $this->assertNotNull($field->getFormatValueCallback());
-        $this->assertTrue($field->getFilterable());
-        $this->assertTrue($field->getFilterable());
-    }
 
-    public function testWrongParameterConstructor()
-    {
-        try {
-            $field = new Field("phone", array(
-                "foo" => "bar"
-            ));
-            $this->fail();
-        } catch (\InvalidArgumentException $e) {
-            $this->assertTrue(true);
-        }
+        
     }
 }
