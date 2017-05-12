5a3b3dce7cf50b2b543db42bb748d2ddda61395f
MOURER Laurent <laurent.mourer@gmail.com>
2017-05-12 13:42:17 +0200

    Update ExerciceTest.php

--- src/UPOND/OrthophonieBundle/Tests/Controller/ExerciceTest.php
+++ src/UPOND/OrthophonieBundle/Tests/Controller/ExerciceTest.php
@@ -10,7 +10,7 @@ class ExerciceTest extends \PHPUnit_Framework_TestCase
         $ec = new Exercice();
         
         $ec->setTempsExercice(1);
-                $result = ec->getTempsExercice(1);
+                $result = $ec->getTempsExercice(1);
                 $this->assertEquals(1, $result);
 
 
