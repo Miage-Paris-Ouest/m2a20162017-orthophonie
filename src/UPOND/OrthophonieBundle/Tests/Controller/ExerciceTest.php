807ac60a08e03d8115044fe3a92308cb7d5f3db6
MOURER Laurent <laurent.mourer@gmail.com>
2017-05-12 11:55:01 +0200

    Update ExerciceTest.php

--- src/UPOND/OrthophonieBundle/Tests/Controller/ExerciceTest.php
+++ src/UPOND/OrthophonieBundle/Tests/Controller/ExerciceTest.php
@@ -1,1 +1,17 @@
+<?php
+namespace UPOND\OrthophonieBundle\Tests\Controller;
+use Symfony\Bundle\FrameworkBundle\Test\TestCase;
+use OrthophonieBundle/Entity/Exercice;
 
+class DefaultControllerTest extends TestCase{
+    public function setUp(){
+      $ex = new Exercice();
+    }
+	
+	private function testGet(){
+    $ex->setIdExercice(1)
+      $this->assertEquals(1, $ex->getIdExercice());
+       
+    }
+
+}
