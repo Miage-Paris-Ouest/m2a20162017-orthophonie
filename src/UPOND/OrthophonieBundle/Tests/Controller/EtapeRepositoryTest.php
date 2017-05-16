4e0e4a144f1b4fc2089f4423b1b90701e1ab10f5
MOURER Laurent <laurent.mourer@gmail.com>
2017-05-16 10:24:20 +0200

    Create EtapeRepositoryTest.php

---
+++ src/UPOND/OrthophonieBundle/Tests/Controller/EtapeRepositoryTest.php
@@ -0,0 +1,44 @@
+namespace Tests\AppBundle\Repository;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ProductRepositoryTest extends KernelTestCase
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        self::bootKernel();
+
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    public function testSearchByCategoryName()
+    {
+        $products = $this->em
+            ->getRepository('AppBundle:Product')
+            ->searchByCategoryName('foo')
+        ;
+
+        $this->assertCount(1, $products);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->em->close();
+        $this->em = null; // avoid memory leaks
+    }
+}
