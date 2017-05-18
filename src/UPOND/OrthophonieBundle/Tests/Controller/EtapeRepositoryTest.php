8c83d23a314abacdb5550de5c1bf9d9d2f419a3a
MOURER Laurent <laurentmourer@mbp-de-laurent.home>
2017-05-18 19:04:13 +0200

    test repo
    test repo

---
+++ src/UPOND/OrthophonieBundle/Tests/Controller/EtapeRepositoryTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laurentmourer
+ * Date: 18/05/2017
+ * Time: 18:59
+ */
+
+namespace UPOND\OrthophonieBundle\Repository;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+
+class EtapeRepositoryTest extends KernelTestCase
+{
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        self::bootKernel();
+
+        $this->em = static::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+    }
+
+    public function testSearchByCategoryName()
+    {
+        $products = $this->em
+            ->getRepository('UPONDOrthophonieBundle:Exercice')
+            ->searchByCategoryName('et.idEtape');
+
+        $this->assertCount(1, $products);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+
+        $this->em->close();
+        $this->em = null; // avoid memory leaks
+    }
+}
+
+
